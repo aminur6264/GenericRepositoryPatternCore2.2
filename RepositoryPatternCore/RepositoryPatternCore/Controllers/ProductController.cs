89f894549b4804c50a356bce56675960417d86c3
Aminur <aminur6264@gmail.com>
2020-10-03 17:55:10 +0600

    Update and Delete Complete

--- RepositoryPatternCore/RepositoryPatternCore/Controllers/ProductController.cs
+++ RepositoryPatternCore/RepositoryPatternCore/Controllers/ProductController.cs
@@ -25,5 +25,47 @@ namespace RepositoryPatternCore.Controllers
                 product = unitOfWork.Products.FirstOrDefault(x => x.Id == id);
             return View(product);
         }
+
+        [HttpPost]
+        public IActionResult NewProduct(Product product)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork();
+            //Product product = new Product();
+            if (product.Id != 0)
+            {
+                Product existProduct = unitOfWork.Products.FirstOrDefault(x => x.Id == product.Id);
+                existProduct.Name = product.Name;
+                existProduct.Brand = product.Brand;
+                existProduct.Price = product.Price;
+
+                unitOfWork.Products.Update(existProduct);
+            }
+            else
+                unitOfWork.Products.Add(product);
+            unitOfWork.Complete();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(int? id)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(); 
+            Product product = new Product();
+            if (id != null)
+                product = unitOfWork.Products.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProduct(Product product)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork();
+            if (product.Id != 0)
+            {
+                Product existsProduct = unitOfWork.Products.FirstOrDefault(x => x.Id == product.Id);
+                unitOfWork.Products.Remove(existsProduct);
+                unitOfWork.Complete();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
